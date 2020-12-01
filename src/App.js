5cd33f03e24784772ea90258a54287b1f6563789
Kharisma Putra <ks.dwiputra@gmail.com>
2020-12-01 12:55:58 +0800

    Add Alert

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   searchUsers = async (text) => {
@@ -21,14 +23,28 @@ class App extends Component {
 
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  showClearButton = () => this.state.users.length > 0 ? true : false
+
+  popAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } })
+
+    setTimeout(() => this.setState({ alert: null }), 2000)
+  }
+
   render() {
-    const { loading, users } = this.state;
+    const { loading, users, alert } = this.state;
 
     return (
       <div className="App">
         <Navbar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClearButton={users.length > 0 ? true : false} />
+          <Alert alert={alert} />
+          <Search 
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClearButton={this.showClearButton()}
+            popAlert={this.popAlert}
+          />
           <Users loading={loading} users={users}/>
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,13 +9,18 @@ export class Search extends Component {
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
-    showClearButton: PropTypes.bool.isRequired
+    showClearButton: PropTypes.bool.isRequired,
+    popAlert: PropTypes.func.isRequired
   };
 
   onSubmit = e => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if (this.state.text === '') {
+      this.props.popAlert('Please enter something', 'light')
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
