368a3a27936d19a3875130fedd9bd0f5036db1a1
Kharisma Putra <ks.dwiputra@gmail.com>
2020-12-01 14:44:01 +0800

    Add User

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function UserItem (props) {
   const { login, avatar_url, html_url } = props.user;
@@ -9,7 +10,7 @@ function UserItem (props) {
       <img src={avatar_url} alt='' className='round-img' style={{ width: '60px' }} />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className='btn btn-dark btn-sm my-1'>More</a>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>More</Link>
       </div>
     </div>
   )
