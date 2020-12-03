3f959c5ee55f8eebdb321cccc233464a788cc98d
Kharisma Putra <ks.dwiputra@gmail.com>
2020-12-03 12:39:51 +0800

    Move pagination to Users

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,20 +1,37 @@
-import React, { useContext } from 'react'
-import UserItem from './UserItem'
-import Spinner from '../layout/Spinner'
+import React, { Fragment, useContext } from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import Pagination from '@material-ui/lab/Pagination';
 import SearchContext from '../../context/search/searchContext';
 
 function Users () {
   const searchContext = useContext(SearchContext);
 
+  const handleChange = (e) => {
+    const selectedPage = e.selected + 1
+
+    searchContext.searchUsers(searchContext.searchText, selectedPage);
+  };
+
   if (searchContext.loading) {
     return <Spinner />
   } else {
     return (
-      <div style={userStyle}>
-        {searchContext.users.map(user =>(
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
+      <Fragment>
+        {searchContext.users.length > 0 && (
+          <Pagination
+            count={searchContext.pageCount}
+            variant="outlined"
+            shape="rounded"
+            onChange={handleChange}
+          />
+        )}
+        <div style={userStyle}>
+          {searchContext.users.map(user =>(
+            <UserItem key={user.id} user={user} />
+          ))}
+        </div>
+      </Fragment>
     )
   }
 }
