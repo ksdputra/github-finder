bbf45b7b61639106732ab4d9afd0e6f6438d91aa
Kharisma Putra <ks.dwiputra@gmail.com>
2020-12-05 15:50:48 +0800

    Update Repos in User

--- src/components/repos/Repos.js
+++ src/components/repos/Repos.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import PropType from 'prop-types';
-import RepoItem from './RepoItem';
+import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 const Repos = ({ repos }) => {
-  return repos.map(repo => <RepoItem repo={repo} key={repo.id} />)
+  return (
+    <Table className='card'>
+      <TableHead>
+        <TableRow>
+          <TableCell>Repository</TableCell>
+          <TableCell>Description</TableCell>
+          <TableCell>Language</TableCell>
+          <TableCell>Updated</TableCell>
+          <TableCell>Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {repos.map((repo) => (
+          <TableRow key={repo.name}>
+            <TableCell>{repo.name}</TableCell>
+            <TableCell>{repo.description}</TableCell>
+            <TableCell>{repo.language}</TableCell>
+            <TableCell>{repo.updated_at}</TableCell>
+            <TableCell><Button variant='contained' color='primary' href={repo.html_url}>Go To Repo</Button></TableCell>
+          </TableRow> 
+        ))}
+      </TableBody>
+    </Table>
+  )
 }
 
 Repos.propTypes = {
