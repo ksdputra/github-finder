3ad3722570a28718fe928d20046290bcb5f56ec4
Kharisma Putra <ks.dwiputra@gmail.com>
2020-12-07 13:33:06 +0800

    Fixing search text when Clear

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -28,6 +28,7 @@ const Search = (props) => {
   const onChange = e => setText(e.target.value);
 
   const clearButtonClicked = e => {
+    setText('')
     searchContext.clearSearch()
     usersContext.clearSearch()
     reposContext.clearSearch()
@@ -36,7 +37,7 @@ const Search = (props) => {
   return (
     <div>
       <form onSubmit={onSubmit} className='form'>
-        <input type='text' name='text' placeholder='Search Users...' value={text} onChange={onChange} />
+        <input type='text' name='text' placeholder='Search something...' value={text} onChange={onChange} />
         <input type='submit' value='Search' className='btn btn-dark btn-block' />
       </form>
       {searchContext.isSearching && (
